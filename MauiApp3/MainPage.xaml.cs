c3e10bc579cbee64f42ad79e3b4d130a01aafb18
Jon Kristensen <jon@jonkr.dk>
2022-09-21 13:44:34 +0200

    Add project files.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		ClassLibrary1.ApiClient client = new ClassLibrary1.ApiClient("http://kartotek.kodeknud.dk", new HttpClient());
+
+		IEnumerable<ClassLibrary1.Person> list = client.PeopleAllAsync().Result;
+
+		myLabel.Text = "";
+		foreach (ClassLibrary1.Person person in list)
+		{
+			myLabel.Text += person.FirstName + " "; ;	
+		}
+	}
+}
+
